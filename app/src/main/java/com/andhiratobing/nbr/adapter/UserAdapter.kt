c46975f102006b408e4698c1cf067e3de3a7445f
ratobing <ratobing6@gmail.com>
2021-10-07 19:40:02 +0700

    Add user adapter

---
+++ app/src/main/java/com/andhiratobing/nbr/adapter/UserAdapter.kt
@@ -0,0 +1,55 @@
+package com.andhiratobing.nbr.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.andhiratobing.nbr.data.model.User
+import com.andhiratobing.nbr.databinding.ItemUserBinding
+import com.bumptech.glide.Glide
+
+class UserAdapter : ListAdapter<User, UserAdapter.UserViewHolder>(USER_COMPARATOR) {
+
+    class UserViewHolder(private val binding: ItemUserBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: User) {
+            binding.apply {
+                tvUsername.text = user.username
+                tvGender.text = user.gender
+                Glide.with(itemView).load(user.avatarUser).into(ivAvatar)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        return UserViewHolder(
+            ItemUserBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val item = getItem(position)
+        if (item != null) {
+            holder.bind(item)
+        }
+    }
+
+    companion object {
+        val USER_COMPARATOR = object : DiffUtil.ItemCallback<User>() {
+            override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+
+}
